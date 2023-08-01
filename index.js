3fe93568fe6cbf27660831d794aced1797ccd43a
Yan <lisska.H@seznam.cz>
2023-08-01 19:57:56 +0200

    Hankie convertor v1.0

---
+++ index.js
@@ -0,0 +1,43 @@
+let original_text = "";
+
+function transformText() {
+    const input_text = document.getElementById("input_text").value;
+    const output_textElement = document.getElementById("output_text");
+
+    let transformed_text = "";
+
+    for (let i = 0; i < input_text.length; i++) {
+        const char = input_text.charAt(i);
+        const random_case = Math.random() < 0.5 ? "toLowerCase" : "toUpperCase";
+        transformed_text += char[random_case]();
+    }
+
+    original_text = input_text;
+
+    output_textElement.value = transformed_text;
+}
+
+function handleKeyDown(event) {
+    if (event.key === "Backspace" || event.key === "Delete") {
+        setTimeout(() => {
+            const transformed_text = document.getElementById("input_text").value;
+            original_text = transformed_text;
+            transformText();
+        }, 0);
+    }
+}
+
+function copyText() {
+    const output_textElement = document.getElementById("output_text");
+    output_textElement.select();
+    document.execCommand("copy");
+
+    const info_textElement = document.getElementById("info_text");
+    info_textElement.style.opacity = "100";
+    info_textElement.style.visibility = "visible";
+
+    setTimeout(() => {
+        info_textElement.style.opacity = "0";
+        info_textElement.style.visibility = "hidden";
+    }, 3000);
+}
